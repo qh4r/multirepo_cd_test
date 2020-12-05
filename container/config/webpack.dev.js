fe4859a3da841eca3be63082d6c29c594bec6a40
Rafał Kucharski <rafal.kucharski@thesoftwarehouse.pl>
2020-12-05 14:03:40 +0100

    Deploy config for container

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require("./webpack.common.js");
 const dependencies = require("../package.json").dependencies;
@@ -20,9 +19,6 @@ const devConfig = {
       },
       //shared: ['react', 'react-dom'],
       shared: dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
     }),
   ],
 };

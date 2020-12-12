9483ea3e565c017248b7aa97856d5406f636389e
Rafał Kucharski <rafal.kucharski@thesoftwarehouse.pl>
2020-12-12 20:31:30 +0100

    Added header

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,14 @@
 import React, {useRef, useEffect} from "react";
 import {MarketingApp} from "./components/MarrketingApp";
+import {BrowserRouter} from "react-router-dom";
+import Header from "./components/Header";
 
 export default () => {
 
-  return <>
-    <h1>Hello!</h1>
+  return <BrowserRouter>
+    <div>
+    <Header />
     <MarketingApp/>
-  </>
+    </div>
+  </BrowserRouter>
 };

0f6c3eef1a3931f1b61c85910bf52029ee49d4e9
Rafał Kucharski <rafal.kucharski@thesoftwarehouse.pl>
2020-12-12 21:11:02 +0100

    fixed header

--- container/src/App.js
+++ container/src/App.js
@@ -2,13 +2,21 @@ import React, {useRef, useEffect} from "react";
 import {MarketingApp} from "./components/MarrketingApp";
 import {BrowserRouter} from "react-router-dom";
 import Header from "./components/Header";
+import {createGenerateClassName, StylesProvider} from "@material-ui/styles";
+
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'cr'
+});
 
 export default () => {
 
   return <BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
     <div>
     <Header />
     <MarketingApp/>
     </div>
+    </StylesProvider>
   </BrowserRouter>
 };
